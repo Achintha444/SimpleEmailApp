4861ca0fda13cbd53adae97394e7a6c34a05262e
Achinths Isuru <achinthaisuru444@gmail.com>
2019-09-18 23:01:23 +0530

    Created the InputTextField in the Contants/Layout folder

---
+++ lib/Constants/Layout/InputTextField.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class InputTextField extends StatelessWidget {
+
+  TextEditingController _controller;
+  bool _obscure;
+  String _labelText;
+
+  InputTextField({TextEditingController controller, bool obscure, String labelText}){
+    this._controller = controller;
+    this._obscure = obscure;
+    this._labelText = labelText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new TextField(
+      controller: this._controller,
+      decoration: new InputDecoration(
+          //hintText: "Enter the password",
+          labelText: this._labelText,
+          //alignLabelWithHint: true,
+          border: new OutlineInputBorder(
+            borderRadius: new BorderRadius.circular(25),
+            gapPadding: 5,
+          )),
+      textAlign: TextAlign.center,
+      obscureText: this._obscure,
+    );
+  }
+}
